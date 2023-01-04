f6588e2f608356eca1fcf9174518fafa0319302a
witwoywhy <but.nuttawit.dev@gmail.com>
2023-01-04 13:01:56 +0700

    feat: cors

--- server/main.go
+++ server/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"net/http"
 	"qr-login/handlers"
 	"qr-login/repositories"
 	"qr-login/services"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -14,6 +16,10 @@ func main() {
 	userHanler := handlers.NewUserHandler(userServ)
 
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:8080"},
+		AllowMethods: []string{http.MethodPost},
+	}))
 	e.POST("/user", userHanler.SignUp)
 	e.Logger.Fatal(e.Start(":8000"))
 }
