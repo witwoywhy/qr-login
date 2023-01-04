d0dd9c88a7ead7e7195c119443ccedacb8f3aadc
witwoywhy <but.nuttawit.dev@gmail.com>
2023-01-04 16:12:14 +0700

    feat: edit header

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,22 +1,31 @@
 import { h } from 'preact';
+import { route } from 'preact-router';
 import { Link } from 'preact-router/match';
 import style from './style.css';
+
+const logout = () => {
+	localStorage.removeItem('user')
+  route('/login', true)
+}
 
 const Header = () => (
 	<header class={style.header}>
 		<a href="/" class={style.logo}>
 			<img src="../../assets/preact-logo-inverse.svg" alt="Preact Logo" />
-			<h1>Preact CLI</h1>
+			<h1>QR Login</h1>
 		</a>
 		<nav>
 			<Link activeClassName={style.active} href="/">
 				Home
 			</Link>
-			<Link activeClassName={style.active} href="/profile">
-				Me
+			<Link activeClassName={style.active} href="/login">
+				Login
 			</Link>
-			<Link activeClassName={style.active} href="/profile/john">
-				John
+			<Link activeClassName={style.active} href="/signup">
+				Sign Up
+			</Link>
+			<Link activeClassName={style.active} onClick={logout}>
+				Logout
 			</Link>
 		</nav>
 	</header>
